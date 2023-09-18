47a7af7b4012decbce9f3e2162f8308070a27998
nttnguyen <nttnguyen136@gmail.com>
2023-09-18 20:56:06 +0700

    config prod

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,14 +2,13 @@ const path = require("path");
 const webpack = require("webpack");
 
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./src/client/index.js",
   mode: "development",
-  output: {
-    libraryTarget: "var",
-    library: "Client",
-  },
+  devtool: "source-map",
+  stats: "verbose",
   module: {
     rules: [
       {
@@ -32,6 +31,12 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: "./src/client/views/index.html",
       filename: "./index.html",
+    }),
+    new CleanWebpackPlugin({
+      dry: true,
+      verbose: true,
+      cleanStaleWebpackAssets: true,
+      protectWebpackAssets: false,
     }),
   ],
 };
